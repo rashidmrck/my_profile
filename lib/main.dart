05cc3af8519e81f335eba95b8bd19bf8db622baf
rashidmrck <rashidmrck@gmail.com>
2022-07-27 19:51:52 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mohammed Rashid C.K',
+      title: 'Mohammed Rashid CK',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
